e1b199ce829b76ef0a54c890b3c7d0a96482d401
Bruno Vieira <bruno.leo516@hotmail.com>
2022-09-30 10:37:26 -0300

    base project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:cep_app/views/map_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'views/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/homePage',
+      routes: {
+        '/homePage': (context) => HomePage(),
+        '/mapsPage': (context) => const MapsPage(),
+      },
+    );
+  }
+}
